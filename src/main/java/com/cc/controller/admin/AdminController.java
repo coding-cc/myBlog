d93e576bd8fa201e40f9ee5ceec31afad77dc2e5
coding-cc <854327790@qq.com>
2019-03-17 15:22:36 +0800

    auto build success!

--- src/main/java/com/cc/controller/admin/AdminController.java
+++ src/main/java/com/cc/controller/admin/AdminController.java
@@ -392,10 +392,4 @@ public class AdminController {
         attachService.save(attach);
         return "succeed";
     }
-
-    @ResponseBody
-    @GetMapping("/out")
-    public String out(){
-        return "ddd";
-    }
 }
